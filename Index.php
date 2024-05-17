ff8b25cc87ad961cef83dd761b52cd9b2fc36617
Peter Overeijnder <peterovereijnder@hotmail.com>
2024-05-17 11:31:20 +0200

    simplified index

--- Index.php
+++ Index.php
@@ -1,45 +1,33 @@
 <?php
 require_once 'UT.php/.init';
-require_once 'Pages/cv.php';
-require_once 'Pages/downloads.php';
-require_once 'Pages/mapviewer.php';
-require_once 'Pages/home.php';
 
 $root = \UT_Php\IO\Directory::fromString(__DIR__);
 
 $router = new \UT_Php\Routing\Router($root, true);
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/', function() { home(); });
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/home', function() { home(); });
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/mapviewer', function() { mapviewer(); });
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/downloads', function() { downloads(); });
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/cv', function() { cv(); });
-
-$router -> match();
-
-function cv()
-{
+$router -> add(\UT_Php\Enums\RequestMethods::Get, '/', function() 
+{ 
     global $root;
-    
-    echo new \Pages\Cv($root);
-}
-
-function downloads()
-{
-    global $root;
-    
-    echo new Pages\Downloads($root);
-}
-
-function mapviewer()
-{
-    global $root;
-    
-    echo new Pages\Mapviewer($root);
-}
-
-function home()
-{
-    global $root;
+    require_once 'Pages/home.php';
     
     echo new \Pages\Home($root);
-}
+});
+$router -> add(\UT_Php\Enums\RequestMethods::Get, '/mapviewer', function() { 
+    global $root;
+    require_once 'Pages/mapviewer.php';
+    
+    echo new Pages\Mapviewer($root);
+});
+$router -> add(\UT_Php\Enums\RequestMethods::Get, '/downloads', function() { 
+    global $root;
+    require_once 'Pages/downloads.php';
+    
+    echo new Pages\Downloads($root);
+});
+$router -> add(\UT_Php\Enums\RequestMethods::Get, '/cv', function() { 
+    global $root;
+    require_once 'Pages/cv.php';
+    
+    echo new \Pages\Cv($root);
+});
+
+$router -> match();
