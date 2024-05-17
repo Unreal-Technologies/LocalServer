e984292de5b111b4e34aeabf3d03476cb6fd1dce
Peter Overeijnder <peterovereijnder@hotmail.com>
2024-05-17 11:47:27 +0200

    dynamicly create base routes

--- Index.php
+++ Index.php
@@ -4,30 +4,24 @@ require_once 'UT.php/.init';
 $root = \UT_Php\IO\Directory::fromString(__DIR__);
 
 $router = new \UT_Php\Routing\Router($root, true);
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/', function() 
-{ 
-    global $root;
-    require_once 'Pages/home.php';
-    
-    echo new \Pages\Home($root);
+$router -> add(\UT_Php\Enums\RequestMethods::Get, '/', function()
+{
+    header('location: /home');
 });
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/mapviewer', function() { 
-    global $root;
-    require_once 'Pages/mapviewer.php';
-    
-    echo new Pages\Mapviewer($root);
-});
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/downloads', function() { 
-    global $root;
-    require_once 'Pages/downloads.php';
-    
-    echo new Pages\Downloads($root);
-});
-$router -> add(\UT_Php\Enums\RequestMethods::Get, '/cv', function() { 
-    global $root;
-    require_once 'Pages/cv.php';
-    
-    echo new \Pages\Cv($root);
-});
+
+foreach(\UT_Php\IO\Directory::fromString('Pages') -> list() as $iDiskManager)
+{
+    if($iDiskManager instanceof UT_Php\Interfaces\IFile && $iDiskManager -> extension() === 'php')
+    {
+        $relative = $iDiskManager -> relativeTo($root);
+        $class = '\\'.str_replace('/', '\\', str_replace('.'.$iDiskManager -> extension() , '', $relative));
+        
+        $router -> add(\UT_Php\Enums\RequestMethods::Get, '/'.$iDiskManager -> basename(), function() use($class, $root, $relative)
+        {
+            require_once $relative;
+            echo new $class($root);
+        });
+    }
+}
 
 $router -> match();
